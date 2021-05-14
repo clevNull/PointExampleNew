5169f427305e37d4a34a2b7945484d0d43023614
devNull <bionttec@gmail.com>
2021-05-14 15:16:30 +0300

    add DbConnWindow and DataGenerationWindow comments

--- PointExample/View/DataGenerationWindow.xaml.cs
+++ PointExample/View/DataGenerationWindow.xaml.cs
@@ -11,64 +11,107 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
-using PointExample.Logicum;
 using System.Threading;
 
+using PointExample.Logicum;
+
+/// <summary>
+/// пространство имен Представление
+/// </summary>
 namespace PointExample.View
 {
     /// <summary>
-    /// Логика взаимодействия для DataGenerationWindow.xaml
+    /// класс описания DataGenerationWindow
     /// </summary>
     public partial class DataGenerationWindow : Window
     {
-        ConncetionLogic dBConn;
-        Logic logicClass;
-        IProgress < string > progressStatus;
+        /// <summary>
+        /// объявляем объект логики подключения к БД/серверу
+        /// </summary>
+        ConncetionLogic mDBConn_;
+        /// <summary>
+        /// объявляем объект логики работы с таблицами в БД
+        /// </summary>
+        Logic mLogicClass_;
+        /// <summary>
+        /// объявляем объект статуса создания заказчиков/заказов
+        /// </summary>
+        IProgress < string > mProgressStatus_;
+
+        /// <summary>
+        /// конструктор по умолчанию
+        /// </summary>
         public DataGenerationWindow()
         {
+            /// инициализируем основные компоненты виджета
             InitializeComponent();
 
-            progressStatus = new Progress<string>(msg => { ProcessStatusOut(msg); });
-
-            
+            /// задаем объект статуса создания заказчиков/заказов
+            mProgressStatus_ = new Progress<string>(msg => { ProcessStatusOut(msg); });
         }
 
+        /// <summary>
+        /// метод обработки нажатия на кнопку "Подключение к БД"
+        /// </summary>
+        /// <param name="sender">объект</param>
+        /// <param name="e">событие</param>
         private void DbConnBtn_Click(object sender, RoutedEventArgs e)
         {
+            /// инициализируем класс логики подключения к БД/серверу
             DbConnWindow dbConnWindow = new DbConnWindow();
+            /// отображаем виджет подключения к БД/серверу
             dbConnWindow.Show();
         }
 
+        /// <summary>
+        /// метод обработки изменения значения слайдера "Заказы"
+        /// </summary>
+        /// <param name="sender">объект</param>
+        /// <param name="e">событие</param>
         private void CountSldr_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            /// задаем значение текстового поля для отображения числа заказов
             OrderBlock.Text = Convert.ToInt32(CountSldr.Value).ToString();
         }
 
+        /// <summary>
+        /// метод обработки нажатия на кнопку "Покупатели"
+        /// </summary>
+        /// <param name="sender">объект</param>
+        /// <param name="e">событие</param>
         private void CustomerSldr_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            /// задаем значение текстового поля для отображения числа Покупателей
             CustomerBlock.Text = Convert.ToInt32(CustomerSldr.Value).ToString();
         }
 
+        /// <summary>
+        /// метод обработки нажатия на кнопку "Сформировать"
+        /// </summary>
+        /// <param name="sender">объект</param>
+        /// <param name="e">событие</param>
         private void DataGenBtn_Click(object sender, RoutedEventArgs e)
         {
-            dBConn = new ConncetionLogic();
-            logicClass = new Logic(Convert.ToInt32(CustomerBlock.Text), Convert.ToInt32(OrderBlock.Text), progressStatus);
+            /// задаем объект логики подключения к БД/серверу
+            mDBConn_ = new ConncetionLogic();
+            /// задаем объект логики работы с таблицами в БД
+            mLogicClass_ = new Logic(Convert.ToInt32(CustomerBlock.Text), Convert.ToInt32(OrderBlock.Text), mProgressStatus_);
             
-            generate();
-        }
-
-        public void generate()
-        {
-            try
-            {
-                logicClass.FillTablesAsync();
-            }
+            /// заполняем таблицы покупателей/заказов
+            try { mLogicClass_.FillTablesAsync(); }
+            /// отлавливаем исключение
             catch (Exception ex)
+            /// выдаем сообщение
             { MessageBox.Show(ex.Message); }
         }
-        public void ProcessStatusOut( string message )
+
+        /// <summary>
+        /// метод задания значения статуса создания покупателей/заказов
+        /// </summary>
+        /// <param name="message"></param>
+        private void ProcessStatusOut( string message )
         {
+            /// задание значения статуса создания покупателей/заказов
             tbStatus.Text = message;
         }
     }
--- PointExample/View/DbConnWindow.xaml.cs
+++ PointExample/View/DbConnWindow.xaml.cs
@@ -15,59 +15,100 @@ using System.Windows.Shapes;
 using PointExample;
 using PointExample.Logicum;
 
+/// <summary>
+/// пространство имен Представление
+/// </summary>
 namespace PointExample.View
 {
     /// <summary>
-    /// Логика взаимодействия для DbConnWindow.xaml
+    /// класс описания DbConnWindow
     /// </summary>
     public partial class DbConnWindow : Window
     {
+        /// <summary>
+        /// инициализируем класс логики подключения к БД/серверу
+        /// </summary>
         ConncetionLogic DbConn = new ConncetionLogic();
+        /// <summary>
+        /// инициализируем класс логики работы с таблицами в БД
+        /// </summary>
         Logic logicClass = new Logic();
 
+        /// <summary>
+        /// конструктор по умолчанию
+        /// </summary>
         public DbConnWindow()
         {
+            /// инициализируем основные компоненты виджета
             InitializeComponent();
         }
 
-
+        /// <summary>
+        /// метод обработки нажатия на кнопку "Подключиться"
+        /// </summary>
+        /// <param name="sender">объект</param>
+        /// <param name="e">событие</param>
         private void SrvConnBtn_Click(object sender, RoutedEventArgs e)
         {
+            /// проверка на пустые поля { хост, юзер, пароль }
             if (addrBox.Text != "" &&
             userNameBox.Text != "" &&
             passwordBox.Password != "")
             {
+                /// подключаемся к серверу
                 try { DbConn.SrvConnect(addrBox.Text, portBox.Text, userNameBox.Text, passwordBox.Password); }
+                /// отлавливаем исключение
                 catch (Exception ex)
+                /// выдаем сообщение
                 { MessageBox.Show(ex.Message); }
-
             }
+            /// выдаем сообщение об некорректности заполнения полей виджета
             else { MessageBox.Show("Поля, необходимые для подключения заполнены не верно/ не заполнены."); }
         }
 
+        /// <summary>
+        /// метод обработки нажатия на кнопку "Создать"
+        /// </summary>
+        /// <param name="sender">объект</param>
+        /// <param name="e">событие</param>
         private void CrtDbBtn_Click(object sender, RoutedEventArgs e)
         {
+            /// проверка на пустые поля { наименование }
             if (nameDbBox.Text != "")
             {
                 try
                 {
+                    /// создаем БД
                     logicClass.DbCreation(nameDbBox.Text);
+                    /// подключаемся к БД
                     DbConn.DbConnect(addrBox.Text, portBox.Text, nameDbBox.Text, userNameBox.Text, passwordBox.Password);
+                    /// создаем таблицы в БД
                     logicClass.TablesCreation();
                 }
+                /// отлавливаем исключение
                 catch (Exception ex)
+                /// выдаем сообщение
                 { MessageBox.Show(ex.Message); }
             }
+            /// выдаем сообщение об некорректности заполнения полей виджета
             else { MessageBox.Show("наименование БД, заполнено не верно/ не заполнено."); }
         }
 
+        /// <summary>
+        /// метод обработки нажатия на кнопку "Удалить"
+        /// </summary>
+        /// <param name="sender">объект</param>
+        /// <param name="e">событие</param>
         private void DelDbBtn_Click(object sender, RoutedEventArgs e)
         {
+            /// проверка на пустые поля { наименование }
             if (nameDbBox.Text != "")
             {
-                try
-                { logicClass.DbDeleting(nameDbBox.Text); }
+                /// удаляем БД
+                try { logicClass.DbDeleting(nameDbBox.Text); }
+                /// отлавливаем исключение
                 catch (Exception ex)
+                /// выдаем исключение
                 { MessageBox.Show(ex.Message); }
             }
         }
